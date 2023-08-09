41890f9984372ae3b574e03f128d9660e237fc3d
ErikSilva231 <erik.silva.fuentes@gmail.com>
2023-08-08 22:58:05 -0400

    stash socialb

--- Desafio-2/src/components/SocialButton.jsx
+++ Desafio-2/src/components/SocialButton.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
 function SocialButton(props) {
+<<<<<<< Updated upstream
 
 
     
     
+=======
+    return (
+    <div>
+        
+    <FontAwesomeIcon icon="fa-brands fa-square-github" />
+    <FontAwesomeIcon icon="fa-brands fa-square-facebook" />
+    <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+    </div>
+    );
+>>>>>>> Stashed changes
 }
 
 export default SocialButton;
